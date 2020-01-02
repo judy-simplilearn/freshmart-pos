54773c63999c337cb0ce657bb90688d7ffa7ee09
Judy <judy.raj@simplilearn.net>
2020-01-02 15:29:23 +0530

    Add project skeleton

---
+++ src/main/java/com/simplilearn/freshmartpos/FreshmartPosApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.freshmartpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshmartPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshmartPosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/freshmartpos/FreshmartPosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.freshmartpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshmartPosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
